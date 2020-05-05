f9f27f51653fb67dfae1490a5aa0552b1f0c4d2b
Scott  Keplinger <skeplin17@gmail.com>
2020-05-05 22:29:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  cartoon_array = ["Bugs Bunny, Daffy Duck, Porky Pig, Taz"]
+  cartoon_array = ["Bugs Bunny", "Daffy Duck", "Porky Pig", "Taz"]
 end
 
 def add_element_to_end_of_array(array, element)
