45c582bb74db789c7e50270ea943236b3f34a25e
Scott  Keplinger <skeplin17@gmail.com>
2020-05-05 22:27:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  new_array = []
+   []
 end
 
 def create_an_array
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  cartoon_arrary << "Tweety Bird"
+  array << "element"
 end
 
 def add_element_to_start_of_array(array, element)
-  cartoon_arrary.unshift ("Foghorn Leghorn"
+  arrary.unshift ("element"
 end
 
 def remove_element_from_end_of_array(array)
-
+  arrary.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  arrary.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  arrary[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+  arrary[first]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array[last]
 end
